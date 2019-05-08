6418e8b36f513b157eadacdddfe1f8e026c2adb4
simon duckett <simon.duckett@fmc.co.uk>
2019-05-08 16:03:29 +0100

    add info section

--- src/Sections/about.js
+++ src/Sections/about.js
@@ -4,6 +4,7 @@ export const About = () => {
     return (
         <section className='min-height'>
             <h2>About</h2>
+            <p>Making beats and instrumentals</p>
         </section>
     )
 }
