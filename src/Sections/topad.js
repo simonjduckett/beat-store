27cb2e120aed01fa599df49f72b799386c3c892d
simon duckett <simon.duckett@fmc.co.uk>
2019-05-19 15:38:43 +0100

    change to 10 free beats

--- src/Sections/topad.js
+++ src/Sections/topad.js
@@ -9,8 +9,8 @@ export class Topad extends Component {
             <section id='top-ad'>
                 <div className='container flex'>
                     <div className='half'>
-                        <h1>Get your hands on 5 free instrumental beats</h1>
-                        <p>Subscribe and be the first to hear about my new music. As a gift, you’ll reveive 5 free beats via instant download!</p>
+                        <h1>Get your hands on 10 free instrumental beats</h1>
+                        <p>Subscribe and be the first to hear about my new music. As a gift, you’ll reveive 10 free beats via instant download!</p>
                         <Button link='https://mailchi.mp/1678769da354/aidronesignup' text='Get your tracks' target='_blank' />
                     </div>
                     <div className='half topad-img'>
